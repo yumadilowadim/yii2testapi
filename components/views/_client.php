7dc1b3664ca803e5a97426c4c602306033005324
Vadim.Y <yumadilowadim@ya.ru>
2022-09-25 15:37:33 +0500

    new migration for clients

--- components/views/_client.php
+++ components/views/_client.php
@@ -1,7 +1,6 @@
 <?php
     $client_car = $model->getClientCar()->one();
 ?>
-
 <td><?php echo $model->id; ?></td>
 <td><?php echo $model->client_name; ?></td>
 <td><?php echo $client_car->car_brand.' '.$client_car->car_model; ?></td>
